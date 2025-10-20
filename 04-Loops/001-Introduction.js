d5566a79e2b85313a88c1a029afb04e89adbd037
Md Warishh <warishmohd987@gmail.com>
2025-10-20 22:22:43 +0530

    Loops Intro

---
+++ 04-Loops/001-Introduction.js
@@ -0,0 +1,36 @@
+
+//*            What is a Loop?
+
+// A loop is a block of code that performs repeated tasks multiple times at a certain condition.
+
+// In simple words, If i have to run a task like 4 times so I write it like this.
+// console.log("A task");
+// console.log("A task");
+// console.log("A task");
+// console.log("A task");
+// But, suppose you have to run the same task like 1000 or 1,00,000 or more times then it is a waste of time & also energy that you write same thing thousand or lakhs time, so here comes ' loops ' that makes that work easier in one line of code.
+
+// * In simple term,  If you have to perform any repeated tasks multiple times, we use loops or recursion.
+// For now we use Loops and in further classes we will also use recursion.
+
+//-----------------------------------------
+
+//*   Types of Loop:-
+
+//?    1) Entry Control Loop
+//?    2) Exit Control Loop
+
+//?  1) Entry Control Loop:
+// In this loop, the condition is checked (asked) before the tasks execute
+// For example: If we go to examination hall we have to show admit card first then they let us give exam. Means the condition checked first then the tasks execute
+
+
+//?  2) Exit Control Loop:
+// In this loop, first its runs but for the second time it checked condition if condition is valid then it will execute if condition is not valid then it will not run
+
+//* In Entry Control we have two types of loop
+//  1)  For  Loop
+//  2)  While Loop
+
+//* In Exit Control we have only one type of loop
+//    =>  do - while loop
